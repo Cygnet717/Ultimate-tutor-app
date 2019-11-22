0d4151892f41cf7db75092398ee246279f153dba
Kathy Bradt <kathybradt717@gmail.com>
2019-11-22 15:42:14 -0600

    header set not functional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <header>
+        <Header />
+      </header>
     </main>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+export default class Header extends Component {
+    render() {
+        return <>
+        <nav className='header'>
+            <h1>
+                <Link to='/'>
+                    Ultimate Tutor
+                </Link>
+            </h1>
+            <Link to='/Login'>
+                Login
+            </Link>
+            <Link to='/Register'>
+                Register
+            </Link>
+        </nav>
+        </>
+    }
+}
