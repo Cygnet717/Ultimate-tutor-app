25c6008dfa020ce858fe5da8e71be6b417dff282
Kathy Bradt <kathybradt717@gmail.com>
2019-12-03 12:02:39 -0600

    Updates

---
+++ precache-manifest.d23d743033fe9d9e0f1a263f75ea6a99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8140c16747135a2379f9321467d79b21",
+    "url": "/Ultimate-tutor-app/index.html"
+  },
+  {
+    "revision": "d46798186ac1f5612d22",
+    "url": "/Ultimate-tutor-app/static/css/main.b9ff5465.chunk.css"
+  },
+  {
+    "revision": "33c16935e814f8841814",
+    "url": "/Ultimate-tutor-app/static/js/2.87a1079c.chunk.js"
+  },
+  {
+    "revision": "d46798186ac1f5612d22",
+    "url": "/Ultimate-tutor-app/static/js/main.5790e344.chunk.js"
+  },
+  {
+    "revision": "0f986ae0aff3af734615",
+    "url": "/Ultimate-tutor-app/static/js/runtime-main.6e7ae290.js"
+  }
+]);
